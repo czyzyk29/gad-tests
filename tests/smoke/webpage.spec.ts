99e9bd2f7182d0d06553398e10327ba84bf2f58b
czyzyk <czyzyk29@wp.pl>
2024-12-11 21:28:53 +0100

    add tags

--- tests/smoke/webpage.spec.ts
+++ tests/smoke/webpage.spec.ts
@@ -4,30 +4,48 @@ import { HomePage } from '../../src/pages/home.page';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify main pages', () => {
-  test('Home page title simple', async ({ page }) => {
-    const homePage = new HomePage(page);
+  test(
+    'Home page title simple',
+    {
+      tag: ['@GAD-R01-01', '@smoke'],
+    },
+    async ({ page }) => {
+      const homePage = new HomePage(page);
 
-    await homePage.goTo();
+      await homePage.goTo();
 
-    const title = await homePage.title();
-    expect(title).toContain('GAD');
-  });
+      const title = await homePage.title();
+      expect(title).toContain('GAD');
+    },
+  );
 
-  test('Articles page title', async ({ page }) => {
-    const articles = new ArticlesPage(page);
+  test(
+    'Articles page title',
+    {
+      tag: ['@GAD-R01-02', '@smoke'],
+    },
+    async ({ page }) => {
+      const articles = new ArticlesPage(page);
 
-    await articles.goTo();
+      await articles.goTo();
 
-    const title = await articles.title();
-    expect(title).toContain('Articles');
-  });
+      const title = await articles.title();
+      expect(title).toContain('Articles');
+    },
+  );
 
-  test('Comments page title', async ({ page }) => {
-    const comments = new CommentsPage(page);
+  test(
+    'Comments page title',
+    {
+      tag: ['@GAD-R01-03', '@smoke'],
+    },
+    async ({ page }) => {
+      const comments = new CommentsPage(page);
 
-    await comments.goTo();
+      await comments.goTo();
 
-    const title = await comments.title();
-    expect(title).toContain('Comments');
-  });
+      const title = await comments.title();
+      expect(title).toContain('Comments');
+    },
+  );
 });
