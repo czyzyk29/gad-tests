be6ec0ef1374aeda14e104f624ef88ff4d98d866
czyzyk <czyzyk29@wp.pl>
2024-12-15 21:31:43 +0100

    add base url

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,8 @@ export default defineConfig({
   workers: undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://localhost:3000',
+    //baseURL: 'http://localhost:3000',
+    baseURL: process.env.BASE_URL,
     actionTimeout: 0,
     trace: 'retain-on-failure',
     video: 'retain-on-failure',
