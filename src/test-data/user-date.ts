5dae881eb7f22ff489fe08bd62cac138864156e8
czyzyk <czyzyk29@wp.pl>
2024-12-15 22:14:44 +0100

    add user password and user login

---
+++ src/test-data/user-date.ts
@@ -0,0 +1,4 @@
+export const testUser1 = {
+  userEmail: process.env.USER_EMAIL ?? 'Not set',
+  userPassword: process.env.USER_PASSWORD ?? 'Not set',
+};
--- tests/smoke/loginpage.spec.ts
+++ tests/smoke/loginpage.spec.ts
@@ -1,10 +1,11 @@
 import { LoginPage } from '../../src/pages/login.page';
 import { WelcomePage } from '../../src/pages/welcome.page';
+import { testUser1 } from '../../src/test-data/user-date';
 import { expect, test } from '@playwright/test';
 
 test('Login with correct credentials', async ({ page }) => {
-  const login = 'Moses.Armstrong@Feest.ca';
-  const password = 'test1';
+  const login = testUser1.userEmail;
+  const password = testUser1.userPassword;
   const loginPage = new LoginPage(page);
   await loginPage.goTo();
 
