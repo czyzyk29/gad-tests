110a49b5f43fb2d2d762c4328023754d64990def
czyzyk <czyzyk29@wp.pl>
2024-12-21 19:38:26 +0100

    add negative test loginpage

--- src/pages/login.page.ts
+++ src/pages/login.page.ts
@@ -3,14 +3,20 @@ import { Page } from '@playwright/test';
 
 export class LoginPage extends BasePage {
   url = '/login/';
+  loginPasswordError = this.page.getByTestId('login-error');
+
+  userEmailInput = this.page.getByPlaceholder('Enter User Email');
+  userPasswordInput = this.page.getByPlaceholder('Enter Password');
+  loginButton = this.page.getByRole('button', { name: 'LogIn' });
+
   constructor(page: Page) {
     super(page);
   }
 
   async login(login: string, password: string): Promise<void> {
-    await this.page.getByPlaceholder('Enter User Email').fill(login);
-    await this.page.getByPlaceholder('Enter Password').fill(password);
+    await this.userEmailInput.fill(login);
+    await this.userPasswordInput.fill(password);
 
-    await this.page.getByRole('button', { name: 'LogIn' }).click();
+    await this.loginButton.click();
   }
 }
--- tests/smoke/loginpage.spec.ts
+++ tests/smoke/loginpage.spec.ts
@@ -3,16 +3,45 @@ import { WelcomePage } from '../../src/pages/welcome.page';
 import { testUser1 } from '../../src/test-data/user-date';
 import { expect, test } from '@playwright/test';
 
-test('Login with correct credentials', async ({ page }) => {
-  const login = testUser1.userEmail;
-  const password = testUser1.userPassword;
-  const loginPage = new LoginPage(page);
-  await loginPage.goTo();
+test.describe('login tests', () => {
+  test(
+    'Login with correct credentials',
+    {
+      tag: ['@GAD-R01-02', '@smoke'],
+    },
+    async ({ page }) => {
+      const login = testUser1.userEmail;
+      const password = testUser1.userPassword;
+      const loginPage = new LoginPage(page);
+      await loginPage.goTo();
 
-  await loginPage.login(login, password);
+      await loginPage.login(login, password);
 
-  const welcomePage = new WelcomePage(page);
-  const title = await welcomePage.title();
+      const welcomePage = new WelcomePage(page);
+      const title = await welcomePage.title();
 
-  expect(title).toContain('Welcome');
+      expect(title).toContain('Welcome');
+    },
+  );
+  test(
+    'Login with incorrect password',
+    {
+      tag: ['@GAD-R01-02', '@smoke'],
+    },
+    async ({ page }) => {
+      const login = testUser1.userEmail;
+      const password = 'wrong_pass';
+      const loginPage = new LoginPage(page);
+      await loginPage.goTo();
+
+      await loginPage.login(login, password);
+
+      await expect
+        .soft(loginPage.loginPasswordError)
+        .toHaveText('Invalid username or password');
+
+      const title = await loginPage.title();
+      expect.soft(title).toContain('Login');
+    },
+  );
 });
