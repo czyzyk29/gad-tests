d7ef0a4779cba8e58627a69a10535e313f353bb9
czyzyk <czyzyk29@wp.pl>
2025-02-23 12:25:58 +0100

    add fixtures pageobject

--- tests/smoke/menu.spec.ts
+++ tests/smoke/menu.spec.ts
@@ -1,24 +1,4 @@
-import { ArticlesPage } from '@_src/pages/articles.page';
-import { CommentsPage } from '@_src/pages/comments.page';
-import { test as baseTest, expect } from '@playwright/test';
-
-interface Pages {
-  articlesPage: ArticlesPage;
-  commentsPage: CommentsPage;
-}
-
-const test = baseTest.extend<Pages>({
-  articlesPage: async ({ page }, use) => {
-    const articlesPage = new ArticlesPage(page);
-    await articlesPage.goTo();
-    await use(articlesPage);
-  },
-  commentsPage: async ({ page }, use) => {
-    const commentsPage = new CommentsPage(page);
-    await commentsPage.goTo();
-    await use(commentsPage);
-  },
-});
+import { expect, test } from '@_src/fixtures/merge-fixtures';
 
 test.describe('Verify main menu buttons', () => {
   test(
