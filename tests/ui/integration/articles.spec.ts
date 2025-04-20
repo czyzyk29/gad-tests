de2b3a8c1e4ed33abda06a0c1015a0f41381c4bf
czyzyk <czyzyk29@wp.pl>
2025-04-20 16:50:09 +0200

    backend communication validation

--- tests/ui/integration/articles.spec.ts
+++ tests/ui/integration/articles.spec.ts
@@ -1,6 +1,6 @@
-import { RESPONSE_TIMEOUT } from '@_pw-config';
 import { prepareRandomArticle } from '@_src/factory/article.factory';
 import { ArticlesPage } from '@_src/pages/articles.page';
+import { waitForResponse } from '@_src/utils/wait.util';
 import { AddArticleView } from '@_src/views/add-article.view';
 import { expect, test } from '@playwright/test';
 
@@ -35,13 +35,7 @@ test.describe('verify tests', () => {
 
       await expect.soft(addArticleView.articleViewHeader).toBeVisible();
 
-      const responsePromise = page.waitForResponse('/api/articles', {
-        timeout: RESPONSE_TIMEOUT,
-      });
-
-      //const body = await response.json();
-
-      //expect(body).toHaveLength(expectDefArticleNumber);
+      const responsePromise = waitForResponse(page, '/api/articles');
 
       //Act
       await addArticleView.createArticle(articleData);
@@ -58,17 +52,22 @@ test.describe('verify tests', () => {
     {
       tag: ['@GAD-R04-02', '@smoke'],
     },
-    async () => {
+    async ({ page }) => {
       //Arrange
       const expectErrorMessage = 'Article was not created';
       const articleData = prepareRandomArticle();
+      const expectResponseCode = 422;
       articleData.title = '';
 
       await expect.soft(addArticleView.articleViewHeader).toBeVisible();
 
+      const responsePromise = waitForResponse(page, '/api/articles');
+
       //Act
       await addArticleView.createArticle(articleData);
+      const response = await responsePromise;
 
+      expect(response.status()).toBe(expectResponseCode);
       //Assert
       await expect(addArticleView.errorPopup).toHaveText(expectErrorMessage);
     },
@@ -80,16 +79,19 @@ test.describe('verify tests', () => {
       {
         tag: ['@GAD-R04-02', '@smoke'],
       },
-      async () => {
+      async ({ page }) => {
         //Arrange
         const expectErrorMessage = 'Article was not created';
         const articleData = prepareRandomArticle(129);
+        const expectResponseCode = 422;
 
         await expect.soft(addArticleView.articleViewHeader).toBeVisible();
-
+        const responsePromise = waitForResponse(page, '/api/articles');
         //Act
         await addArticleView.createArticle(articleData);
+        const response = await responsePromise;
 
+        expect(response.status()).toBe(expectResponseCode);
         //Assert
         await expect(addArticleView.errorPopup).toHaveText(expectErrorMessage);
       },
@@ -100,15 +102,17 @@ test.describe('verify tests', () => {
       {
         tag: ['@GAD-R04-02', '@smoke'],
       },
-      async () => {
+      async ({ page }) => {
         //Arrange
         const articleData = prepareRandomArticle(128);
-
+        const expectResponseCode = 201;
         await expect.soft(addArticleView.articleViewHeader).toBeVisible();
-
+        const responsePromise = waitForResponse(page, '/api/articles');
         //Act
         const articlePage = await addArticleView.createArticle(articleData);
+        const response = await responsePromise;
 
+        expect(response.status()).toBe(expectResponseCode);
         //Assert
         await expect
           .soft(articlePage.articleTitle)
--- tests/ui/integration/search.spec.ts
+++ tests/ui/integration/search.spec.ts
@@ -1,11 +1,12 @@
 import { expect, test } from '@_src/fixtures/merge-fixtures';
+import { waitForResponse } from '@_src/utils/wait.util';
 
 test.describe('verify tests', () => {
   test('go button show', async ({ articlesPage, page }) => {
     await expect(articlesPage.goSearchButton).toBeInViewport();
 
     const expectDefArticleNumber = 6;
-    const responsePromise = page.waitForResponse('/api/articles*');
+    const responsePromise = waitForResponse(page, '/api/articles*');
 
     await articlesPage.goSearchButton.click();
     const response = await responsePromise;
